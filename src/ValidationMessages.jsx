29d3b6e2a1720145f18e81857cb4fce7a4ddde5f
Guillaume Gomez <guillaume.gomez846@gmail.com>
2019-01-11 16:06:45 +0100

    update style (need more improvements) validatationMessages component

--- src/ValidationMessages.jsx
+++ src/ValidationMessages.jsx
@@ -1,33 +1,40 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 import keys from 'lodash/keys';
+import { withStyles } from '@material-ui/core/styles';
 import filter from 'lodash/filter';
 
-const validationStyles = {};
+const validationStyles = {
+  errorList: {
+    backgroundColor: "#e9e9e9",
+    color: "#f44336",
+    listStyle:"none"
+  },
+};
 
-const Validation = ({ validation }) => (
-  <div>
-    <p>{validation.message}</p>
-  </div>
+const Validation = ({ validation, classes }) => (
+  <Typography variant="p">
+    {validation.message}
+  </Typography>
 );
 
-const Validations = ({ validation }) => (
-  <div>
-    {validation.map((v, idx) => (<Validation key={idx} validation={v} />)) // eslint-disable-line react/no-array-index-key,max-len
+const Validations = ({ validation, classes }) => (
+  <li className={classes.ul} >
+    {validation.map((v, idx) => (<Validation key={idx} validation={v} classes={classes} />)) // eslint-disable-line react/no-array-index-key,max-len
     }
-  </div>
+  </li>
 );
 
-const ValidationMessages = ({ validation }) => (
-  <div>
+const ValidationMessages = ({ validation, classes }) => (
+  <ul className={classes.errorList}>
     {validation && filter(keys(validation), (k) => {
       const v = validation[k];
       return v && v.length > 0;
     }).map(v => (
-      <Validations key={v} validation={validation[v]} />
+      <Validations key={v} validation={validation[v]} classes={classes} />
     ))
     }
-  </div>
+  </ul>
 );
 
 export default withStyles(validationStyles)(ValidationMessages);
