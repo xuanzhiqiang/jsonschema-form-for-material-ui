4e6b16e7c7d642533b868a526601fde305a17c0a
Asbjørn Lucassen <asbjorn.lucassen@ssf.no>
2019-03-15 15:37:43 +0100

    date input label always hovers to avoid overlapping with browser placeholder

--- src/fields/configure/get-label-component-props.js
+++ src/fields/configure/get-label-component-props.js
@@ -1,9 +1,14 @@
 import includes from 'lodash/includes';
 
-export default ({ htmlId, required, path }) => {
-  const rv = {
+export default ({ htmlId, required, path, schema }) => {
+  let rv = {
     htmlFor: htmlId,
-    required: includes(required, path),
+    required: includes(required, path)
   };
+
+  if (schema.type === 'date') {
+    rv = { ...rv, shrink: true };
+  }
+
   return rv;
 };
