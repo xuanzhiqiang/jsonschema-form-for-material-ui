ac93018ff52d8dbf55cbd4e274d6517ad5840799
zhiqiang.xuan <zhiqiang.xuan@linkplay.com>
2019-06-05 12:58:34 +0800

    fix bug

--- src/fields/components/Image.jsx
+++ src/fields/components/Image.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
 export default ({ alt, src, height, width = '100%' }) => (
-  <img src={src} alt={alt} height={height} width={width} />
+  <dev>
+    <img src={src} alt={alt} height={height} width={width} />
+  </dev>
 );
