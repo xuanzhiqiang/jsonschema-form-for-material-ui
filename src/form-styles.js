a9d90013f82a01b5099b4ec0974a8563ff3d0442
zhiqiang.xuan <zhiqiang.xuan@linkplay.com>
2019-06-05 11:41:24 +0800

    fix bug

--- src/form-styles.js
+++ src/form-styles.js
@@ -1,23 +1,25 @@
 export default theme => ({
   root: {
-    padding: theme.spacing.unit * 2,
+    padding: theme.spacing.unit * 2
   },
   formButtons: {
+    display: 'flex',
     marginTop: theme.spacing.unit * 2,
-    justifyContent: 'flex-end',
+    justifyContent: 'flex-end'
   },
   submit: {
-    fontSize: '100%',
+    fontSize: '100%'
   },
   cancel: {
-    fontSize: '100%',
+    fontSize: '100%'
   },
   button: {
     fontSize: '100%',
+    margin: theme.spacing.unit * 2
   },
   field: {
     display: 'flex',
-    flexDirection: 'column',
+    flexDirection: 'column'
   },
-  formfield: {},
+  formfield: {}
 });
