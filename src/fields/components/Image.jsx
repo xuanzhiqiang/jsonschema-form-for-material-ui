03dd2626d53636aa71ad78612b58a8c2be2fbfa2
zhiqiang.xuan <zhiqiang.xuan@linkplay.com>
2019-08-12 20:01:46 +0800

    [other] Image 增加点击预览

--- src/fields/components/Image.jsx
+++ src/fields/components/Image.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import ModalImage from 'react-modal-image';
 
-export default ({ alt, src, height = 'auto', width = '100%' }) => (
+export default ({ alt, src }) => (
   <div style={{ marginTop: '1.5em' }}>
-    <img src={src} alt={alt} height={height} width={width} />
+    {/* <img src={src} alt={alt} height={height} width={width} /> */}
+    <ModalImage small={src} large={src} alt={alt} />
   </div>
 );
